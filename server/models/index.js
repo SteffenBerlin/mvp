c339b88b4a550987c25b0a6c3bf8365c7d2cd007
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-02-15 22:26:05 -0800

    Create commitments get function

--- server/models/index.js
+++ server/models/index.js
@@ -11,10 +11,33 @@ module.exports = {
       .then(function(userdata){
         callback(JSON.stringify(userdata));
       }, function(err){
-        console.log(err);
+        console.log('This is the user get error: ' + err);
       })
     },
-    
+    post: function ( userdata ) {
+      var newUser = User.build( userdata );
+      newUser.save().then(function(){
+        console.log('Insert ' + userdata.username + 'into users');
+      },
+      function(err){
+        console.log('This is the Users POST error: ' + err);
+      });
+    }
+  },
 
+  commitments: {
+    get: function ( username, callback ){
+      Commitment.findAll({where: {username: username}
+    })
+    .then(function(commitments){
+      callback(JSON.stringify(commitments));
+    }, function(err) {
+      console.log('This is the commitments get error: ' + err);
+      })
+    },
+
+    post: function () {
+
+    }
   }
 }
