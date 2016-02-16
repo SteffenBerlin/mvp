90230b44b0015fe7317b14793e711f1860bf100c
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-02-16 00:50:04 -0800

    Create server router

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,14 @@
 var controller = require('./controllers');
 var router = require('express').Router();
 
-//routes
+router.get('/users', controller.users.get);
+
+router.post('/users', controller.users.post);
+
+router.put('/users', controller.users.update);
+
+router.get('/commitments', controller.commitments.get);
+
+router.post('/commitments', controller.commitments.post);
 
 module.exports = router;
